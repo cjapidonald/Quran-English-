e60506c7982314375add52f786d54e7ded8cd234
donaldcjapi <dcjapi@gmail.com>
2025-10-07 07:32:52 +0700

    Initial Commit

---
+++ Quran English/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Quran English
+//
+//  Created by Donald Cjapi on 7/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
